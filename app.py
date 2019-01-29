5995539e0bb357168049325893913681eddaec18
Trentham3269 <scottdalwood@gmail.com>
2019-01-29 11:01:50 +0000

    Move secret key definition for deployment

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ import requests
 app = Flask(__name__)
 
 
+# Set the secret key to some random bytes
+app.secret_key = "super secret key"
+
+
+# Client id for Google oauth
 client_id = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 application_name = 'Catalog App'
@@ -345,6 +350,5 @@ def itemAPI(name, description):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
